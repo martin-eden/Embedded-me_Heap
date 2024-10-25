11fd4055d15afed2c9010875dbd2130c74f02c5d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-25 15:33:29 +0200

    [*] [Remove]

--- Remove.sh
+++ Remove.sh
@@ -5,10 +5,10 @@ arduino-cli lib uninstall me_Heap
 
 # Uninstall dependencies
 arduino-cli lib uninstall \
-  https://github.com/martin-eden/Embedded-me_Bits \
-  https://github.com/martin-eden/Embedded-me_MemorySegment \
-  https://github.com/martin-eden/Embedded-me_ManagedMemory \
-  https://github.com/martin-eden/Embedded-me_Console \
-  https://github.com/martin-eden/Embedded-me_InstallStandardStreams \
-  https://github.com/martin-eden/Embedded-me_UartSpeeds \
-  https://github.com/martin-eden/Embedded-me_BaseTypes
+  me_Bits \
+  me_MemorySegment \
+  me_ManagedMemory \
+  me_Console \
+  me_InstallStandardStreams \
+  me_UartSpeeds \
+  me_BaseTypes
