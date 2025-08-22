224262419fbe160837badc4e271910ddf36d454b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-22 21:15:50 +0200

    [^~] [Example] Sync with current modules.

--- examples/me_Heap/me_Heap.ino
+++ examples/me_Heap/me_Heap.ino
@@ -2,34 +2,14 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-12-19
+  Last mod.: 2025-08-22
 */
 
 #include <me_Heap.h>
 
 #include <me_BaseTypes.h>
-#include <me_Uart.h>
 #include <me_Console.h>
 #include <me_MemorySegment.h>
-#include <me_InstallStandardStreams.h> // Implementation uses printf()
-
-void setup()
-{
-  me_Uart::Init(me_Uart::Speed_115k_Bps);
-  InstallStandardStreams();
-
-  Console.Print("[me_Heap] Start.");
-  Console.Indent();
-  RunTest();
-  Console.Unindent();
-  Console.Print("[me_Heap] Done.");
-}
-
-void loop()
-{
-}
-
-// --
 
 void RunTest()
 {
@@ -84,8 +64,24 @@ void RunTest()
   Heap.Release(&ms_2);
 }
 
+void setup()
+{
+  Console.Init();
+
+  Console.Print("[me_Heap] Start.");
+  Console.Indent();
+  RunTest();
+  Console.Unindent();
+  Console.Print("[me_Heap] Done.");
+}
+
+void loop()
+{
+}
+
 /*
   2024-10-11
   2024-10-12
   2024-10-25
+  2025-08-22
 */
